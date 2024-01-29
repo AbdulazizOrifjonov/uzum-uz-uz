8b67fa6557b2b1e2af6feb2d1bc65c56ae08185f
AbdulazizLegend <abdulazizbekorifjonov6@gmail.com>
2024-01-29 20:32:02 +0500

    first commit

---
+++ src/Components/NavbarBottom/NavbarBottom.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./NavbarBottom.css"
+function NavbarBottom() {
+  return (
+    <div>
+        NavbarBottom
+      
+    </div>
+  )
+}
+
+export default NavbarBottom
---
+++ src/Components/NavbarMain/NavbarMain.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import SubHeader from '../SubHeader/SubHeader'
+import NavbarBottom from '../NavbarBottom/NavbarBottom'
+
+function NavbarMain() {
+  return (
+    <>
+        <SubHeader/>
+        <Navbar/>
+        <NavbarBottom/>
+    </>
+  )
+}
+
+export default NavbarMain
