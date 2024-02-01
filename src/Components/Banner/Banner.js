9a3641a5e3d5b761060e5d4d302a0514f5d2e76a
AbdulazizLegend <abdulazizbekorifjonov6@gmail.com>
2024-02-01 13:44:08 +0500

    sdf

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import banner from "../../Assets/banner.jpg"
+import "./Banner.css"
+
 function Banner() {
   return (
-    <div className='container'>
+        <div className="banner">
+
         <img src={banner} alt="" />
-    </div>
+        </div>
   )
 }
 
